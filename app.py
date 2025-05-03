40b8623fb5725b7507c79c3eb79899a7e1b6f6aa
Aarav Bamba <82966356+aaravbmb@users.noreply.github.com>
2025-05-03 11:59:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def analyze_page():
         transcription_engine = st.radio(
             "Select an emotion model:",
             options=["Custom Model", "Whisper"],
-            captions=["LSTM Model made using Tensorflow","OpenAI's audio analysis model."]
+            captions=["LSTM Model made using Tensorflow","OpenAI's audio analysis model."],
             index=0,
             horizontal=True,
             label_visibility="hidden"
